6224c091484d6122977777c5bdb1aaac96c2fd49
Kochetkov Alex <leshakochetcov@gmail.com>
2026-06-04 23:06:40 +0300

    Add bcrypt and other things

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-    has_many :quotes, dependent: :destroy
-    
+  has_many :quotes, dependent: :destroy
+  has_secure_password
+  validates :name, presence: true, uniqueness: true
+  validates :password, length: { minimum: 8 }, allow_nil: true
 end
