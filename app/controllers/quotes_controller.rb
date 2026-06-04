4a6a280ffc655db3c7c9cfa84f1cd6cfcdefd695
KovalenkoRita <margo.kvlnk@yandex.ru>
2026-06-04 22:01:36 +0300

    logout

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,6 @@
 class QuotesController < ApplicationController
-  before_action :require_login
-  
+  # before_action :require_login
+
   def index
     # Инкапсулируем логику: берем цитаты текущего пользователя
     # Для начала можно написать просто Quote.all, если юзеров еще нет
